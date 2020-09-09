449c8b1bd8ca7305a97748adb6effccf6f65c5f2
Игорь Гиголаев <gigolaevigor@mail.ru>
2020-09-09 10:38:40 +0300

    Простая модуляция

--- js/overlay.js
+++ js/overlay.js
@@ -1,32 +1,34 @@
-var overlay = document.querySelector('.gallery-overlay');
-var currentPicture = document.querySelector('.picture');
-var keksPictures = document.querySelectorAll('.pictures .picture');
-var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
-
-showBigPicture = function(picture){
-    overlay.classList.remove('hidden');
-    // console.log(picture)
-    var img = picture.querySelector('img').getAttribute('src');
-    var likes = 20;
-    var comments =39;
-
-    // console.log(img);
-
-    overlay.querySelector('.gallery-overlay-image').setAttribute('src', img);
-    overlay.querySelector('.likes-count').textContent = likes;
-    overlay.querySelector('.gallery-overlay-controls-comments').textContent = comments;
-}
-
-// showBigPicture(currentPicture);
-
-for(let i = 0; i< keksPictures.length; i++){
-    keksPictures[i].addEventListener('click', function(evt){
+(function(){
+    var overlay = document.querySelector('.gallery-overlay');
+    var currentPicture = document.querySelector('.picture');
+    var keksPictures = document.querySelectorAll('.pictures .picture');
+    var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+    
+    showBigPicture = function(picture){
+        overlay.classList.remove('hidden');
+        // console.log(picture)
+        var img = picture.querySelector('img').getAttribute('src');
+        var likes = 20;
+        var comments =39;
+    
+        // console.log(img);
+    
+        overlay.querySelector('.gallery-overlay-image').setAttribute('src', img);
+        overlay.querySelector('.likes-count').textContent = likes;
+        overlay.querySelector('.gallery-overlay-controls-comments').textContent = comments;
+    }
+    
+    // showBigPicture(currentPicture);
+    
+    for(let i = 0; i< keksPictures.length; i++){
+        keksPictures[i].addEventListener('click', function(evt){
+            evt.preventDefault();
+            showBigPicture(evt.currentTarget);
+        })
+    }
+    
+    galleryOverlayClose.addEventListener('click', function(evt){
         evt.preventDefault();
-        showBigPicture(evt.currentTarget);
+        overlay.classList.add('hidden');
     })
-}
-
-galleryOverlayClose.addEventListener('click', function(evt){
-    evt.preventDefault();
-    overlay.classList.add('hidden');
-})
+}())
